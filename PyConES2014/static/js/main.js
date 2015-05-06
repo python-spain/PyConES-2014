cd28487b9100b26d0a2f8a2884ffdef7b209045a
David Francos <me@davidfrancos.net>
2015-05-06 17:52:24 +0000

    Internacionalizacion con menús en condiciones

--- PyConES2014/static/js/main.js
+++ PyConES2014/static/js/main.js
@@ -1,3 +1,13 @@
+String.prototype.count = function(search) {
+        var m = this.match(new RegExp(search.toString().replace(/(?=[.\\+*?[^\]$(){}\|])/g, "\\"), "g"));
+            return m ? m.length:0;
+}
+
 function select_lang() {
-    window.location.href = URI(window.location.href).search({'lang': $.trim($('#langselect option:selected').val())});
+    lang = $.trim($('#langselect option:selected').val());
+    if (window.location.href.count("/") > 3){
+        window.location.href = window.location.protocol + "//" + window.location.hostname + "/"+lang+"/" + window.location.href.split("/")[4]
+    } else {
+        window.location.href = window.location.protocol + "//" + window.location.hostname + "/"+lang+"/";
+    }
 }
